5e8ecb5e7cc5f1ef60ee6fb2bb1ec3f1147dc23f
Aaron Heesakkers <aaronheesakkers@gmail.com>
2017-07-11 13:14:53 +0200

    Customize interface through configuration.

--- bin/cli.js
+++ bin/cli.js
@@ -7,11 +7,32 @@ const prompt = require('../libs/prompt')
 const Whale = require('../index')
 
 let exchangers = require('../config/exchangers.json');
+let config     = require('../config/whale.json');
+
+// deepMerge to use built-in config as defaults.
+const deepMerge = function (target, source) {
+  for (var p in source) {
+    if (!target.hasOwnProperty(p)) { // set
+      target[p] = source[p];
+    } else if (target[p] instanceof Object) { // recurse
+      deepMerge(target[p], source[p])
+    }
+  }
+  return target;
+}
+
 if (args.config) {
   if (!fs.existsSync(args.config)) {
     return console.error(`config file ${args.config} does not exist`);
   }
-  exchangers = require(args.config);
+  // Detect combined config.
+  let _config = require(args.config);
+  if (_config.hasOwnProperty('exchangers')) {
+    exchangers = _config.exchangers;
+  }
+  if (_config.hasOwnProperty('whale')) {
+    config = deepMerge(_config.whale, config);
+  }
 }
 
 prompt(exchangers, args.all).then((answer) => {
@@ -29,5 +50,5 @@ prompt(exchangers, args.all).then((answer) => {
   const exchange = exchangers[answer.exchange];
   exchange.name = answer.exchange;
 
-  new Whale(args.seconds, exchange, answer.markets)
+  new Whale(config, exchange, answer.markets)
 }).catch(console.error)
