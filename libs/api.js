fef2bbfbdc8c8513a51ebaabf694e1f54f041aa5
Aaron Heesakkers <aaronheesakkers@gmail.com>
2017-07-11 16:56:17 +0200

    preserve markets order

--- libs/api.js
+++ libs/api.js
@@ -1,7 +1,20 @@
 const sea = require('./sea')
 
 exports.getCurrentPrice = function(exchange, markets) {
-  return sea.getCurrentPrice(exchange, markets)
+  return new Promise((resolve) => {
+    sea.getCurrentPrice(exchange, markets).then((data) => {
+
+      // Reorder data to match specified markets order.
+      let dataOrdered = [];
+      markets.forEach(market => {
+        for (var i=0; i<data.length; i++) {
+          if (data[i].name === market) { dataOrdered.push(data[i]); break; }
+        }
+      });
+
+      resolve(dataOrdered);
+    });
+  });
 }
 
 exports.getPriceTrend = function(exchange, currentMarket) {
